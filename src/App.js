6eb417da8c2383583b08d0025b676c5459a715b3
toxa16 <toxa16@gmail.com>
2019-10-17 22:57:18 +0300

    main block margin box

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Main />
+      <div style={{ margin: '1rem' }}>
+        <Main />
+      </div>
       <Footer />
     </div>
   );
