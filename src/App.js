a84bbdfcafce599b17bdd27acc4400e327c1215d
toxa16 <toxa16@gmail.com>
2019-10-17 17:17:51 +0300

    init footer component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container } from '@material-ui/core';
 
 import './App.css';
+import Footer from './Footer/Footer';
 
 function App() {
   return (
@@ -12,29 +13,7 @@ function App() {
       <main className="App-Main">
         <Container>Main</Container>
       </main>
-      <footer className="App-Footer">
-        <Container>
-          Footer
-          <table>
-            <thead>
-              <tr>
-                <th>Geschwindigkeit</th>
-                <th>Bereich</th>
-                <th>Benzin</th>
-                <th>Temperatur</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>5,65 km/h</td>
-                <td>0,2  ha/h</td>
-                <td>10 lt</td>
-                <td>95 °C</td>
-              </tr>
-            </tbody>
-          </table>
-        </Container>
-      </footer>
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer/Footer.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Container } from '@material-ui/core';
+
+import './Footer.css';
+
+export default function Footer() {
+  return (
+    <footer className="Footer">
+      <Container>
+        <table className="Footer-Table">
+          <thead>
+            <tr>
+              <th className="Footer-Th">Geschwindigkeit</th>
+              <th className="Footer-Th">Bereich</th>
+              <th className="Footer-Th">Benzin</th>
+              <th className="Footer-Th">Temperatur</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="Footer-Td">
+                <span>5,65</span>
+                {' '}
+                <span className="Footer-Td_extraLight">km/h</span>
+              </td>
+              <td className="Footer-Td">
+                <span>0,2</span>
+                {' '}
+                <span className="Footer-Td_extraLight">ha/h</span>
+              </td>
+              <td className="Footer-Td">
+                <span>10</span>
+                {' '}
+                <span className="Footer-Td_extraLight">lt</span>
+              </td>
+              <td className="Footer-Td">
+                <span>95</span>
+                {' '}
+                <span className="Footer-Td_extraLight">°C</span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </Container>
+    </footer>
+  );
+}
