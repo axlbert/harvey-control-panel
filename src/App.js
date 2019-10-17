2d8f4c25f7d2aa720ab638a48d90ef48dd523750
toxa16 <toxa16@gmail.com>
2019-10-17 15:25:56 +0300

    init global markup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Container } from '@material-ui/core';
 
 import './App.css';
 
@@ -6,8 +7,34 @@ function App() {
   return (
     <div className="App">
       <header className="App-Header">
-        <span>harvey.one</span>
+        <Container>harvey.one</Container>
       </header>
+      <main className="App-Main">
+        <Container>Main</Container>
+      </main>
+      <footer className="App-Footer">
+        <Container>
+          Footer
+          <table>
+            <thead>
+              <tr>
+                <th>Geschwindigkeit</th>
+                <th>Bereich</th>
+                <th>Benzin</th>
+                <th>Temperatur</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>5,65 km/h</td>
+                <td>0,2  ha/h</td>
+                <td>10 lt</td>
+                <td>95 °C</td>
+              </tr>
+            </tbody>
+          </table>
+        </Container>
+      </footer>
     </div>
   );
 }
