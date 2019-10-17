67be54e392403b628db8e3a9686ab8849480db2f
toxa16 <toxa16@gmail.com>
2019-10-17 18:42:43 +0300

    update main

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -3,6 +3,7 @@ import { Container } from '@material-ui/core';
 
 import './Main.css';
 import onOff from './on-off.png';
+import arrowButton from './arrow-button.png';
 
 export default function Main() {
   return (
@@ -11,15 +12,22 @@ export default function Main() {
         <div className="Main-Inner">
           <div className="Main-Left">
             <div className="Belts">Belts</div>
-            <div className="Main-Controls">
+            <div className="Main-Controls" style={{ marginTop: '2rem' }}>
               <img src={onOff} alt="On/Off" />
             </div>
           </div>
           <div className="Main-Right">
-            <span className="Main-Text">GPS Map</span>
+            <div className="Main-Text" style={{ marginBottom: '.5rem' }}>
+              GPS Map
+            </div>
             <div className="Main-VideoWrapper"></div>
             <div className="Main-VideoSeparator"></div>
             <div className="Main-VideoWrapper"></div>
+            <div className="Main-VideoControls">
+              <img src={arrowButton} alt="Prev" />
+              <span className="Main-Text">Camera Front</span>
+              <img src={arrowButton} alt="Next" />
+            </div>
           </div>
         </div>
       </Container>
