bedca6f1964260c6d9a75453e518288b631281b9
toxa16 <toxa16@gmail.com>
2019-10-18 17:59:59 +0300

    on-off button state colors

--- src/Main/OnOffButton/OnOffButton.js
+++ src/Main/OnOffButton/OnOffButton.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import './OnOffButton.css';
 import onOffGreen from './on-off-green.png';
+import onOffRed from './on-off-red.png';
+import onOffGray from './on-off-gray.png';
 
 export default function OnOffButton({ large }) {
   function handleClick() {
     console.log('On/Off clicked.');
   }
-  
+
   const buttonClassName = `OnOffButton ${large && 'OnOffButton_large'}`;
   return (
     <button className={buttonClassName} onClick={handleClick}>
