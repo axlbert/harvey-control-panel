44ba56516ad14aae6852a347bab2091b90222455
toxa16 <toxa16@gmail.com>
2019-10-18 20:04:53 +0300

    update arrow button

--- src/Main/ArrowButton/ArrowButton.js
+++ src/Main/ArrowButton/ArrowButton.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
-import image from './arrow-button.png';
+import './ArrowButton.css';
+import gray from './button-gray.png';
 
 export default function ArrowButton() {
+  function handleClick() {
+    console.log('arrow button clicked');
+  }
+
+  const buttonStyle = { backgroundImage: `url(${gray})` };
   return (
-    <div>
-      <img src={image} alt="ArrowButton" />
-    </div>
+    <button className="ArrowButton" style={buttonStyle} onClick={handleClick}>
+      <span className="ArrowButton-Icon material-icons">
+        keyboard_arrow_up
+      </span>
+    </button>
   );
 }
