5d51d728834e15d112ee490dd8e6d2fbee642466
toxa16 <toxa16@gmail.com>
2019-10-18 16:25:54 +0300

    update belt

--- src/Main/Belts/Belt/Belt.js
+++ src/Main/Belts/Belt/Belt.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import { Slider } from '@material-ui/core';
 
 import './Belt.css';
 
 import onOff from './on-off-small.png';
 
 export default function Belt() {
+  const [value, setValue] = React.useState(30);
+
+  const handleChange = (event, newValue) => {
+    console.log(newValue);
+    setValue(newValue);
+  };
+
   return (
     <div className="Belt">
       <img src={onOff} />
-      <div>Belt 1</div>
-      <div>Slider</div>
+      <div className="Belt-Separator"></div>
+      <div className="Belt-Name">Belt 1</div>
+      <div className="Belt-SliderContainer">
+        <div className="Belt-SliderMark">-</div>
+        <Slider value={value} onChange={handleChange} />
+        <div className="Belt-SliderMark">+</div>
+      </div>
     </div>
   );
 }
