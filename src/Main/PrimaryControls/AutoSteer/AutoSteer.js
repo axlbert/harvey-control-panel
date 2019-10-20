e6c9683c1f906ebf19c4b7c08923e8b7ef2f9529
toxa16 <toxa16@gmail.com>
2019-10-20 23:46:31 +0300

    init custom switch component

--- src/Main/PrimaryControls/AutoSteer/AutoSteer.js
+++ src/Main/PrimaryControls/AutoSteer/AutoSteer.js
@@ -1,9 +1,6 @@
 import React, { useState } from 'react';
 
-import './AutoSteer.css';
-
-import gray from './thumb-gray.png';
-import green from './thumb-green.png';
+import CustomSwitch from './CustomSwitch';
 
 export default function AutoSteer() {
   const [active, setActive] = useState(false);
@@ -12,21 +9,11 @@ export default function AutoSteer() {
     setActive(x => !x);
   }
 
-  const thumbClassName = 
-    `CustomSwitch-Thumb ${active && 'CustomSwitch-Thumb_active'}`;
-  const thumbImgSrc = active ? green : gray;
   return (
-    <div>
-      <div className="CustomSwitch" onClick={handleToggle}>
-        <div className="CustomSwitch-Track">
-          <img
-            className={thumbClassName}
-            src={thumbImgSrc}
-            alt="Toggle Auto-Steer"
-          />
-        </div>
-        <div className="CustomSwitch-Label">Auto-Steer</div>
-      </div>
-    </div>
+    <CustomSwitch
+      label="Auto-Steer"
+      checked={active}
+      onChange={handleToggle}
+    />
   );
 }
--- src/Main/PrimaryControls/AutoSteer/CustomSwitch.js
+++ src/Main/PrimaryControls/AutoSteer/CustomSwitch.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
-export default function CustomSwitch() {
+import './CustomSwitch.css';
+import gray from './thumb-gray.png';
+import green from './thumb-green.png';
+
+export default function CustomSwitch({ label, checked, onChange }) {
+  const thumbClassName = 
+    `CustomSwitch-Thumb ${checked && 'CustomSwitch-Thumb_active'}`;
+  const thumbImgSrc = checked ? green : gray;
   return (
-    <div>Switch</div>
+    <div className="CustomSwitch" onClick={onChange}>
+      <div className="CustomSwitch-Track">
+        <img
+          className={thumbClassName}
+          src={thumbImgSrc}
+          alt={`Toggle ${label}`}
+        />
+      </div>
+      <div className="CustomSwitch-Label">{ label }</div>
+    </div>
   );
 }
