9f0dd09b4faa158589120ad12195ce7f7ade4416
toxa16 <toxa16@gmail.com>
2019-10-21 01:34:03 +0300

    update login form markup

--- src/LoginScreen/LoginForm/LoginForm.js
+++ src/LoginScreen/LoginForm/LoginForm.js
@@ -3,24 +3,49 @@ import React from 'react';
 import './LoginForm.css';
 import envelope from './envelope.png';
 import lock from './lock.png';
+import button from './button.png';
 
 export default function LoginForm() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log('submitted');
+  }
+
   return (
-    <form className="LoginForm">
+    <form className="LoginForm" onSubmit={handleSubmit}>
       <div className="LoginForm-InputContainer" style={{ marginTop: '3rem' }}>
-        <label className="LoginForm-InputLabel">
-          <img src={envelope} alt="Envelope" />
-          <span style={{ marginLeft: '.75rem' }}>e-mail</span>
+        <label className="LoginForm-InputLabel" htmlFor="email">
+          <img src={envelope} alt="Envelope" style={{ marginTop: 3 }} />
+          <span className="LoginForm-InputLabelText">
+            e-mail
+          </span>
         </label>
-        <input className="LoginForm-Input" />
+        <input
+          className="LoginForm-Input"
+          type="email"
+          name="email"
+          placeholder="hello@schmiede.one"
+        />
       </div>
-      <div className="LoginForm-InputContainer" style={{ marginTop: '2rem' }}>
-        <label className="LoginForm-InputLabel">
+
+      <div className="LoginForm-InputContainer" style={{ marginTop: '1.95rem' }}>
+        <label className="LoginForm-InputLabel" htmlFor="password">
           <img src={lock} alt="Lock" />
-          <span style={{ marginLeft: '.75rem' }}>passwort</span>
+          <span className="LoginForm-InputLabelText">
+            passwort
+          </span>
         </label>
-        <input className="LoginForm-Input" />
+        <input
+          className="LoginForm-Input"
+          type="password"
+          name="password"
+          placeholder="************"
+        />
       </div>
+
+      <button className="LoginForm-SubmitButton" type="submit">
+        Einloggen
+      </button>
     </form>
   );
 }
